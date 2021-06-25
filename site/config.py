62bb4734fd6c05c021e46acc3111f24ee1af3b6a
Olav Schettler <olav@schettler.net>
2021-06-25 15:39:05 +0200

    Empty site

---
+++ site/config.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------------------
+# Site Configuration File
+# ------------------------------------------------------------------------------
+
+# Choose the theme to use when building your site. This variable should specify
+# the name of a theme directory in your site's 'lib' folder.
+theme = "graphite"
+
+# Site title.
+title = "Podcasts by DW (Deutsche Welle)"
+
+# Site tagline.
+tagline = "German and European news, analysis, opinion and breaking news from Germany's international broadcaster."
